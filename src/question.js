a62593e6afde2ec1cb5cc283d3bd5b328ba361cb
Sieur Loic <loic.tramis@gmail.com>
2024-05-20 17:53:06 +0200

    Add: day1 and day2 method

--- src/question.js
+++ src/question.js
@@ -1,7 +1,39 @@
 class Question {
     // YOUR CODE HERE:
-    //
     // 1. constructor (text, choices, answer, difficulty)
-
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
     // 2. shuffleChoices()
+    shuffleChoices() {
+        /*
+    -- To shuffle an array a of n elements (indices 0..n-1):
+        for i from n−1 down to 1 do
+            j ← random integer such that 0 ≤ j ≤ i
+            exchange a[j] and a[i]
+        */
+        for (let index = 0; index < this.choices.length; index++) {
+            let randomIndex = Math.floor(Math.random() * this.choices.length);
+            if (randomIndex === index) {
+                index--;
+                continue;
+            }
+            let temp = this.choices[index];
+            this.choices[index] = this.choices[randomIndex];
+            this.choices[randomIndex] = temp;
+        }
+    }
 }
+
+// For testing with: node src/question.js
+/* const q = new Question("", [1, 2, 3, 4, 5, 6, 7, 8, 9], "", "");
+console.log(q.choices);
+let i = 0;
+while (i < 100) {
+    q.shuffleChoices();
+    console.log(q.choices);
+    i++;
+} */
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,56 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
     // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
 
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
+    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        return this.currentQuestionIndex++;
+    }
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+        return this.questions;
+    }
 
     // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        if (this.questions[this.currentQuestionIndex].answer === answer) {
+            this.correctAnswers++;
+        }
+    }
 
     // 6. hasEnded()
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        } else if (this.currentQuestionIndex === this.questions.length) {
+            return true;
+        }
+    }
+
+    filterQuestionsByDifficulty(difficulty) {
+        if (!(1 <= difficulty <= 3) || typeof difficulty !== "number") {
+            return null;
+        } else {
+            this.questions = this.questions.filter((question) => question.difficulty === difficulty);
+        }
+    }
+    averageDifficulty() {
+        let sum = this.questions.reduce((acc, curr) => acc + curr.difficulty, 0);
+        return sum / this.questions.length;
+    }
 }
